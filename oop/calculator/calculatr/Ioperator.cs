a777b26740bbf868969d9a015a189d13043420c7
mainul hasan <mainulhasan@iut-dhaka.edu>
2024-01-07 18:14:09 +0600

    calculatr

---
+++ oop/calculator/calculatr/Ioperator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatr
+{
+    public interface IOperator
+    {
+        double Calculate(double num1, double num2);
+    }
+    public class Add : IOperator
+    {
+        public double Calculate(double num1, double num2)
+        {
+            return num1+num2;
+        }
+    }
+    public class Subtract : IOperator
+    {
+        public double Calculate(double num1, double num2)
+        {
+            return num1-num2;
+        }
+    }
+    public class Multiply : IOperator
+    {
+        public double Calculate(double num1, double num2)
+        {
+            return num1*num2;
+        }
+    }
+    public class Divide : IOperator
+    {
+        public double Calculate(double num1, double num2)
+        {
+            try
+            {
+                if (num2 == 0)
+                {
+                    throw new DivideByZeroException("Cannot divide by zero!");
+                }
+                return num1/num2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ oop/calculator/calculatr/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatr
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            Console.Write("Enter your choice (1-4): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            IOperator operate;
+            switch (choice)
+            {
+                case 1:
+                    operate = new Add(); 
+                    break;
+                case 2:
+                    operate = new Subtract();
+                    break;
+                case 3:
+                    operate = new Multiply();
+                    break;
+                case 4:
+                    operate = new Divide();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    return;
+            }
+
+            double result = operate.Calculate(num1,num2);
+            Console.WriteLine($"Result: {result}");
+        }
+
+       
+    }
+
+}
